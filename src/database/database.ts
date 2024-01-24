8b6cd3d3abc3516b0812fc09722e9d9312c541cb
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-24 20:09:48 +0100

    Dependencie mysql2 + configuration

---
+++ src/database/database.ts
@@ -0,0 +1,12 @@
+import mysql2, { ConnectionOptions } from 'mysql2'
+
+const config:ConnectionOptions = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'hpshop',
+    connectionLimit: 10,
+    waitForConnections: true,
+    queueLimit: 0
+}
+
