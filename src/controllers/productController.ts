6892539a200b0144ea7cfe96622a98bdc03ece1c
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-24 20:50:47 +0100

    getAllProducts from ProductController completed

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,11 +1,16 @@
 import { Request, Response} from 'express'
 import productService from '../services/productService'
+import { sendError, sendSuccess } from 'utils/requestHandlers'
 
 class ProductController {
 
     async getAllProducts(_req: Request, res: Response){
-        const products = await productService.getAllProducts()
-        res.json(products)
+        try {
+            const products = await productService.getAllProducts()
+            sendSuccess(res, products)
+        } catch (error:any) {
+            sendError(res, error.message) // Using 'error.message' is not the better way.
+        }
     }
 
 }
