764b75403ddcec747572c865e380115326196181
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-24 21:03:59 +0100

    Route getById created and prove it

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -4,6 +4,7 @@ import productController from "../controllers/productController";
 const productRouter = Router()
 
 productRouter.get('/', productController.getAllProducts)
+productRouter.get('/:id', productController.getProductById)
 
 
 export default productRouter
