bf1d2c508f3e573d07a1d8817c81790322608100
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-24 21:18:17 +0100

    putProduct route and test it, works fine :)

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -6,6 +6,7 @@ const productRouter = Router()
 productRouter.get('/', productController.getAllProducts)
 productRouter.get('/:id', productController.getProductById)
 productRouter.post('/', productController.postProduct)
+productRouter.put('/:id', productController.putProduct)
 
 
 export default productRouter
