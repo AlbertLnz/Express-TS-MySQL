ecf4b85689be6f2d5984bd801abc23ab87bdee2b
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-24 21:05:56 +0100

    RowDdataPacket -> GET ~ ResultSetHeader -> POST, PUT, DELETE

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,15 +1,16 @@
 import db from '../database/database'
 import { Product } from '../models/product'
+import { RowDataPacket, ResultSetHeader } from 'mysql2'
 
 class ProductService {
 
     async getAllProducts(): Promise<Product[]>{
-        const products = await db.query('SELECT * FROM Products')
+        const products = await db.query<RowDataPacket[]>('SELECT * FROM Products')
         return products as Product[]
     }
 
     async getProductById(id:number): Promise<Product | null>{
-        const product = await db.query('SELECT * FROM Products WHERE id = ?', id)
+        const product = await db.query<RowDataPacket[]>('SELECT * FROM Products WHERE id = ?', id)
         
         if(Array.isArray(product) && product.length > 0){
             return product[0] as Product
