87766c41fab5abc3c4b57b2bb5b97ada7b287e24
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-24 20:03:45 +0100

    Example endpoint GET Hello World

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,9 @@ import express from 'express'
 const app = express()
 app.use(express.json())
 
-
+app.get('/', (_req, res) => {
+    res.send('Hello World!')
+})
 
 
 const PORT = process.env.PORT || 3000
