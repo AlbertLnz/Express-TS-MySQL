6bda455edd1655630302e3e5cf5651e4a36cf8c3
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-24 20:58:28 +0100

    getProductById service created

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -8,6 +8,16 @@ class ProductService {
         return products as Product[]
     }
 
+    async getProductById(id:number): Promise<Product | null>{
+        const product = await db.query('SELECT * FROM Products WHERE id = ?', id)
+        
+        if(Array.isArray(product) && product.length > 0){
+            return product[0] as Product
+        }
+
+        return null
+    }
+
 }
 
 export default new ProductService()
