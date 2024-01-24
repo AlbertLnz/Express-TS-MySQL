6ed4f665b96efa2826dbd530d616403812b8ea98
AlbertLnz <albert2000_lanza@hotmail.com>
2024-01-24 21:23:57 +0100

    deleteProduct route & test, all working

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -7,6 +7,7 @@ productRouter.get('/', productController.getAllProducts)
 productRouter.get('/:id', productController.getProductById)
 productRouter.post('/', productController.postProduct)
 productRouter.put('/:id', productController.putProduct)
+productRouter.delete('/:id', productController.deleteProduct)
 
 
 export default productRouter
